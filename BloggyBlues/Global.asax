b69612aa2c4531e795eb30622d6aa6fc5165da4b
Brad Minch <minch.bradley@gmail.com>
2016-07-25 14:39:29 -0400

    Add project files.

---
+++ BloggyBlues/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BloggyBlues.MvcApplication" Language="C#" %>
---
+++ BloggyBlues/Models/Post.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace BloggyBlues.Models
+{
+    public class Post
+    {
+
+        public int ID { get; set; }
+
+
+        [StringLength(60, MinimumLength = 1)]
+        public string Title { get; set; }
+
+
+        public string Author { get; set; }
+        
+
+        public string Content { get; set; }
+
+
+        public DateTime PublishedDate { get; set; }
+    }
+
+    public class PostDBContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+    }
+}
