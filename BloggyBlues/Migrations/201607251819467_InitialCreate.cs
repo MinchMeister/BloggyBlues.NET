810a7f15996b85a5994d5dc9ba9b2d7de44d9d5a
Brad Minch <minch.bradley@gmail.com>
2016-07-26 12:28:08 -0400

    LAST PUSH BEFORE ADDING COMMENTS

---
+++ BloggyBlues/Migrations/201607251819467_InitialCreate.cs
@@ -0,0 +1,29 @@
+namespace BloggyBlues.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Posts",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Title = c.String(maxLength: 60),
+                        Author = c.String(),
+                        Content = c.String(),
+                        PublishedDate = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Posts");
+        }
+    }
+}
---
+++ BloggyBlues/Models/Comment.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace BloggyBlues.Models
+{
+    public class Comment
+    {
+        public int ID { get; set; }
+
+
+        [StringLength(60, MinimumLength = 1)]
+        public string Title { get; set; }
+
+
+        public string Author { get; set; }
+
+
+        public string Content { get; set; }
+
+
+        public DateTime PublishedDate { get; set; }
+    }
+
+    public class CommentDBContext : DbContext
+    {
+        public DbSet<Comment> Commments { get; set; }
+    }
+}
